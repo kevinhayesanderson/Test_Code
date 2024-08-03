8e40f1aa09b345290997d8e966eb1fd9495471af
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-08-03 10:48:42 +0530

    Added InvalidDataCheck

---
+++ InvalidDataCheck/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace InvalidDataCheck
+{
+    internal static class Program
+    {
+        private static void Main(string[] _)
+        {
+            Console.WriteLine("Value    IsInvalidData");
+            double dbl = default;
+            Console.WriteLine($"{dbl}  {IsInvalidData(dbl)}");
+            double? ndbl1 = default;
+            Console.WriteLine($"{ndbl1}  {IsInvalidData(ndbl1)}");
+            double? ndbl2 = double.NaN;
+            Console.WriteLine($"{ndbl2}  {IsInvalidData(ndbl2)}");
+
+            Console.ReadKey();
+        }
+
+        private static bool IsInvalidData(object? data)
+        {
+            bool res = data == null || IsNaN(data);
+            return res;
+
+            static bool IsNaN(object data) => data switch
+            {
+                double dbl => double.IsNaN(dbl),
+                float flt => float.IsNaN(flt),
+                ulong => false,
+                uint => false,
+                ushort => false,
+                long => false,
+                int => false,
+                short => false,
+                byte => false,
+                sbyte => false,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        ////Output:
+        ////Value   IsInvalidData
+        ////0       False
+        ////        True
+        ////NaN     True
+    }
+}
+
+
